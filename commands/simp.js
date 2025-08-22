779cec53033145c063075874f45f6806f27e9d96
nimanew3031 <nimanewbot@gmail.com>
2025-08-22 22:49:30 +0530

    Update simp.js

--- commands/simp.js
+++ commands/simp.js
@@ -38,7 +38,7 @@ async function simpCommand(sock, chatId, quotedMsg, mentionedJid, sender) {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '@newsletter',
-                    newsletterName: '𝐉ᴜɴᴇ 𝐌ᴅ',
+                    newsletterName: 'nimesha',
                     serverMessageId: -1
                 }
             }
