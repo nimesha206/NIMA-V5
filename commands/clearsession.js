d172b3aeb0eb0a7e3a3954fcbea03b60ef266bf3
nimanew3031 <nimanewbot@gmail.com>
2025-08-22 21:41:26 +0530

    Update clearsession.js

--- commands/clearsession.js
+++ commands/clearsession.js
@@ -7,8 +7,8 @@ const channelInfo = {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterJid: '120363419075720962@newsletter',
+            newsletterName: 'nimesha',
             serverMessageId: -1
         }
     }
